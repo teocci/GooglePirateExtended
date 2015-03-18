1fc67740837fc4be85cd7035213a5e23e679450e
Jorge Frisancho <jrfdeft@gmail.com>
2015-03-18 15:48:19 +0900

    Create GooglePirateExtendedv4.meta.js

---
+++ dist/GooglePirateExtendedv4.meta.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @id          GooglePirateExtendedv4
+// @name        GooglePirateExtendedv4
+// @namespace	https://openuserjs.org/scripts/teocci/GooglePirateExtendedv4
+// @description	Use Google to find free music, tv shows, movies, anime, torrents, comics, books and other files stored publicly online.
+// @version		4.0.1
+// @author      Jorge Frisancho <teocci@hanmail.net>
+// @include		http://*.google.*/*
+// @include		https://*.google.*/*
+// @include		http://*.google.*/webhp*
+// @include		https://*.google.*/webhp*
+// @include		http://*.google.*/search*
+// @include		https://*.google.*/search*
+// @include		https://encrypted.google.*/search* 
+// @include		https://encrypted.google.*/ 
+// @include		*google.*/firefox*
+// @exclude		*images.google*
+// @exclude		*video.google*
+// @grant		none
+// @updateURL       https://openuserjs.org/scripts/teocci/GooglePirateExtendedv4.meta.js
+// @downloadURL     https://openuserjs.org/scripts/teocci/GooglePirateExtendedv4.user.js
+// @run-at          document-start
+// @updateVersion   155
+// @priority        9001
+// @contributionURL https://github.com/teocci/GooglePirateExtended/wiki/Donate
+// @license         MIT
+// ==/UserScript==
